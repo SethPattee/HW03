3cca104d45a6ae660771c4d73e8577efb4d32cda
setpa <setpa@DESKTOP-1NH481P>
2024-02-01 12:58:33 -0800

    Add project files.

---
+++ HW03/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HW03
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
